4cc8ebe802e14e88199aae03d1375815f58f3459
Rajvardhan Oak <rvoak@berkeley.edu>
2021-07-15 18:02:01 -0700

    Added new function

--- src/utilities.py
+++ src/utilities.py
@@ -5,4 +5,8 @@ import numpy as np
 def get_random_integer():
   random_int = np.random.randint(100, size = 1)
   return random_int[0]
+  
+def get_random_integer_array(upper_bound = 100, arr_size = 10):
+  random_ints = np.random.randint(upper_bound, size = arr_size)
+  return random_ints
 
